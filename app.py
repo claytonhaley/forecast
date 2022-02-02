eaf9e914eddf23c3758c57a99defa79dd9f48783
claytonhaley <cch0045@auburn.edu>
2022-02-01 18:01:42 -0600

    Define new model structure

--- app.py
+++ app.py
@@ -7,191 +7,12 @@ import yfinance as yf
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
+from keras_tuner.tuners import RandomSearch
 from keras.callbacks import EarlyStopping
-from keras.models import Sequential
+from keras.models import Model, Sequential
 from keras.layers import LSTM, Dense, Dropout
 from tensorflow.keras.optimizers import SGD
-
-
-# ---- PREPROCESS ----
-def _preprocess(ticker, start, end):
-    """
-    Loads the ticker data, selects columns for training, and scales the data appropriately.
-    
-            Parameters:
-                    ticker   (string): stock ticker
-                    start    (string): string formatted date
-                    end      (string): string formatted date
-            Returns:
-                    scaler   (scaler obj): scaler obj used later
-                    df       (pd.DataFrame): dataframe without Date column 
-                    dates    (pd.Series): dates from dataframe
-                    final_df (pd.DataFrame): dataframe used for training
-            Except:
-                    st.error: Ticker not available message
-    """
-
-    try:
-        df = pdr.get_data_yahoo(f"{ticker}", start=f"{start}", end=f"{end}")
-    except:
-        st.error("Ticker Not Available")
-    
-    df = df[['High', 'Low', 'Open', 'Volume', 'Close']]
-    df.reset_index(inplace=True)
-
-    dates = df['Date']
-    df = df.drop('Date', axis=1)
-
-    # Scale the data and create training dataset
-    scaler = StandardScaler()
-    final_df = scaler.fit_transform(df)
-    return scaler, df, dates, final_df
-
-
-# ---- CREATE TRAINING DATASET ----
-def _create_dataset(dataset, future_days, past_days):
-    """
-    Creates training datasets based on the number of lookback days and prediction days.
-    
-            Parameters:
-                    dataset      (np.ndarray): stock ticker
-                    future_days  (int): string formatted date
-                    past_days    (int): string formatted date
-            Returns:
-                    X_train      (np.ndarray): input values
-                    y_train      (np.ndarray): target values 
-    """
-
-    X_train, y_train = [], []
-    for i in range(past_days, len(dataset) - future_days + 1):
-        X_train.append(dataset[i - past_days:i, 0:dataset.shape[1] - 1])
-        y_train.append(dataset[i + future_days - 1:i + future_days, 4])
-
-    X_train, y_train = np.array(X_train), np.array(y_train)
-
-    return X_train, y_train
-
-
-# ---- BUILD LSTM NEURAL NETWORK ----
-def _build_model(data, lookback_days, n_epochs, X_train, y_train):
-    """
-    Creates training datasets based on the number of lookback days and prediction days.
-    
-            Parameters:
-                    data            (np.ndarray): training data
-                    lookback_days   (int): days considered for training
-                    n_epochs        (int): training epochs
-                    X_train         (np.ndarray): input values
-                    y_train         (np.ndarray): target values
-            Returns:
-                    model           (model obj): trained model
-                    history:        (history obj): model history/details
-    """
-
-    model = Sequential()
-    model.add(LSTM(64, activation='relu', input_shape=(lookback_days, data.shape[1]-1), return_sequences=True))
-    model.add(LSTM(10, activation='relu', return_sequences=False))
-    model.add(Dropout(0.2))
-    model.add(Dense(1))
-
-    model.compile(optimizer = SGD(learning_rate=0.01, momentum=0.9, clipnorm=1.0), loss='mean_squared_error')
-    
-    early_stop = EarlyStopping(monitor='val_loss', min_delta=1e-10, patience=10, verbose=1)
-    
-    history = model.fit(X_train, y_train, epochs=n_epochs, callbacks=early_stop,
-                            batch_size=16, validation_split=0.1, verbose=0)
-
-    return model, history
-
-
-# ---- GENERATE PREDICTIONS ----
-def _generate_predictions(data, model, scaler, dates, 
-                            n_future, n_back, X_train, y_train):
-    """
-    Creates training datasets based on the number of lookback days and prediction days.
-    
-            Parameters:
-                    data                       (np.ndarray): training data
-                    model                      (model obj): trained model
-                    scaler                     (scaler obj): scaler used to reverse transformed values
-                    dates                      (np.ndarray): input values
-                    n_future                   (int): number of future days predicted
-                    n_back                     (int): number of considered past days
-                    X_train                    (np.ndarray): input values
-                    y_train                    (np.ndarray): target values
-            Returns:
-                    mse                        (float): root mean squared error
-                    PREDICTIONS_FUTURE         (pd.DataFrame): future predictions
-                    PREDICTION_TRAIN           (pd.DataFrame): training predictions
-    """                    
-
-    # Generate all y_train values
-    y_train_copies = np.repeat(y_train, data.shape[1], axis=-1)
-    y_train_actual = scaler.inverse_transform(y_train_copies)[:,0]
-    
-    # Generate all y_train predictions
-    all_predictions_train = model.predict(X_train)
-    all_y_train_pred_copies = np.repeat(all_predictions_train, data.shape[1], axis=-1)
-    all_y_train_pred_actual = scaler.inverse_transform(all_y_train_pred_copies)[:,0]
-
-    # Generate future and previous predictions based on lookback and future days
-    predictions_future = model.predict(X_train[-n_future:])
-    predictions_train = model.predict(X_train[n_back:])
-
-    # Inverse transform predictions_train
-    train_predict_copies = np.repeat(predictions_train, data.shape[1], axis=-1)
-    y_pred_train = scaler.inverse_transform(train_predict_copies)[:,0]
-
-    # Inverse transform predictions_future
-    train_future_copies = np.repeat(predictions_future, data.shape[1], axis=-1)
-    y_pred_future = scaler.inverse_transform(train_future_copies)[:,0]
-
-    # Dates for future predictions
-    datelist_future = pd.date_range(list(dates)[-1], periods=n_future, freq='1d').tolist()
-
-    # Final data frames for plotting
-    PREDICTIONS_FUTURE = pd.DataFrame(y_pred_future, columns=['Close']).set_index(pd.Series(datelist_future))
-    PREDICTION_TRAIN = pd.DataFrame(y_pred_train, columns=['Close']).set_index(pd.Series(dates[2 * n_back + n_future - 1:]))
-
-    # Calculate RMSE
-    rmse = math.sqrt(mean_squared_error(y_train_actual, all_y_train_pred_actual))
-
-    return rmse, PREDICTIONS_FUTURE, PREDICTION_TRAIN
-
-
-def _pred_plot(df, dates, start_date_plot, future_preds, train_preds):
-    """
-    Creates training datasets based on the number of lookback days and prediction days.
-    
-            Parameters:
-                    df                       (pd.DataFrame): original dataframe
-                    dates                    (pd.Series): dates from dataframe
-                    start_date_plot          (string): date to start plot
-                    future_preds             (pd.DataFrame): future predictions
-                    train_preds              (pd.DataFrame): training predictions
-            Returns:
-                    st.pyplot(fig)
-    """
-
-    dates = list(dates)
-    df['Date'] = dates
-    df.set_index('Date', inplace=True)
-    
-    fig = plt.figure()
-    plt.plot(future_preds.index, future_preds['Close'], color='r', label='Predicted Stock Price')
-    plt.plot(train_preds.loc[start_date_plot:].index, train_preds.loc[start_date_plot:]['Close'], color='orange', label='Training predictions')
-    plt.plot(df.loc[start_date_plot:].index, df.loc[start_date_plot:]['Close'], color='b', label='Actual Stock Price')
-
-    plt.axvline(x = min(future_preds.index), color='green', linewidth=2, linestyle='--')
-
-    plt.grid(which='major', color='#cccccc', alpha=0.5)
-
-    plt.legend(shadow=True)
-    plt.title('Predcitions and Actual Stock Prices', family='Arial', fontsize=18)
-    plt.xlabel('Timeline', family='Arial', fontsize=14)
-    plt.ylabel('Stock Price Value', family='Arial', fontsize=14)
-    plt.xticks(rotation=45, fontsize=8)
-    st.pyplot(fig)
+from model import ModelIntegration
 
 
 # -------- MAIN FUNCTION --------
@@ -223,42 +44,53 @@ def main():
         epochs = st.sidebar.slider("Epochs", min_value=1, max_value=100)
 
         submit = st.form_submit_button('Run Model')
+        tune_choice = st.checkbox('Hyperparameter Tuning')
+        tuner = False
+
+        if tune_choice:
+            tuner = True
 
         if submit:
-            try:
-                # ---- DATA PREPROCESSING ----
-                scaler, original_df, all_dates, final_df = _preprocess(ticker_name, start_date, end_date)
+            # try:
+            # ---- DATA PREPROCESSING ----
+            integrate = ModelIntegration(ticker_name, start_date, end_date, lookback_days, lookahead_days, epochs, tuner)
 
-                X_train, y_train = _create_dataset(final_df, lookahead_days, lookback_days)
+            integrate._preprocess()
+
+            integrate._create_dataset()
+        
+            # Instantiate Model Architecture
+            with st.spinner('Training Model...'):
+                if tuner == False:
+                    model, history = integrate._build_default_model()
+                else:
+                    model, history = integrate._run_tuned_model()
+
             
-                # Instantiate Model Architecture
-                with st.spinner('Training Model...'):
-                    model, history = _build_model(final_df, lookback_days, epochs, X_train, y_train)
+            with left_column:
+                fig = plt.figure()
+                plt.plot(history.history['loss'], label='Training loss')
+                plt.plot(history.history['val_loss'], label='Validation loss')
+                plt.legend()
+                st.pyplot(fig)
+
+            # Generating Predictions
+            with st.spinner('Generating Predictions...'):
+                rmse, r2, _, _ = integrate._generate_predictions(model)
+
+                with right_column:
+                    integrate._pred_plot()
+
+            st.success('Done!')
+
+            # Generating RMSE
+            with left_column:
+                st.warning(f"Stopped at {len(history.history['loss'])} epochs (loss failed to improve)")
+                st.info(f"RMSE: {rmse}")
+                st.info("R-Squared:" + "{:.2%}".format(r2))
                 
-                with left_column:
-                    fig = plt.figure()
-                    plt.plot(history.history['loss'], label='Training loss')
-                    plt.plot(history.history['val_loss'], label='Validation loss')
-                    plt.legend()
-                    st.pyplot(fig)
-
-                # Generating Predictions
-                with st.spinner('Generating Predictions...'):
-                    rmse, future_predictions, train_predictions = _generate_predictions(original_df, model, scaler, all_dates, 
-                                                                lookahead_days, lookback_days, X_train, y_train)
-
-                    with right_column:
-                        _pred_plot(original_df, all_dates, start_date, future_predictions, train_predictions)
-
-                st.success('Done!')
-
-                # Generating RMSE
-                with left_column:
-                    st.warning(f"Stopped at {len(history.history['loss'])} epochs (loss failed to improve)")
-                    st.info(f"RMSE: {rmse}")
-                
-            except:
-                st.error("Please choose the appropriate parameters")
+            # except:
+            #     st.error("Please choose the appropriate parameters")
 
 
 if __name__ == "__main__":
---
+++ model.py
@@ -0,0 +1,249 @@
+import math
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, r2_score
+from keras_tuner.tuners import RandomSearch
+from keras_tuner.engine.hyperparameters import HyperParameters
+from keras_tuner import HyperModel
+from keras.callbacks import EarlyStopping
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Conv1D, MaxPooling1D
+from tensorflow.keras.optimizers import Adam
+
+
+class ModelIntegration(HyperModel):
+
+    def __init__(self, ticker_name, start_date, end_date, 
+                    lookback_days, lookahead_days, epochs, tuner):
+        """
+
+        """
+        self.ticker_name = ticker_name
+        self.start_date = start_date
+        self.end_date = end_date
+        self.lookback_days = lookback_days
+        self.lookahead_days = lookahead_days
+        self.epochs = epochs
+        self.tuner = tuner
+
+    # ---- PREPROCESS ----
+    def _preprocess(self):
+        """
+        Loads the ticker data, selects columns for training, and scales the data appropriately.
+        
+                Parameters:
+                        ticker   (string): stock ticker
+                        start    (string): string formatted date
+                        end      (string): string formatted date
+                Returns:
+                        scaler   (scaler obj): scaler obj used later
+                        df       (pd.DataFrame): dataframe without Date column 
+                        dates    (pd.Series): dates from dataframe
+                        final_df (pd.DataFrame): dataframe used for training
+                Except:
+                        st.error: Ticker not available message
+        """
+
+        try:
+            yahoo_financials = YahooFinancials(f"{self.ticker_name}")           
+        except:
+            st.error("Ticker Not Available")
+        
+        data = yahoo_financials.get_historical_price_data(f"{self.start_date}", f"{self.end_date}", "daily")
+        full_data = pd.DataFrame(data[f"{self.ticker_name}"]['prices'])
+
+        self.dates = full_data['formatted_date']
+        self.df = full_data[['high', 'low', 'open', 'volume', 'adjclose', 'close']]
+
+        return self.df, self.dates, self.scaler
+
+
+    # ---- CREATE TRAINING DATASET ----
+    def _create_dataset(self):
+        """
+        Creates training datasets based on the number of lookback days and prediction days.
+        
+                Parameters:
+                        dataset      (np.ndarray): stock ticker
+                        future_days  (int): string formatted date
+                        past_days    (int): string formatted date
+                Returns:
+                        X_train      (np.ndarray): input values
+                        y_train      (np.ndarray): target values 
+        """
+        dataset = self.df.to_numpy()
+        X_scaler = MinMaxScaler(feature_range=(-1,1))
+        X_scaled = X_scaler.fit_transform(dataset[:,0:dataset.shape[1]-1])
+
+        y_scaler = MinMaxScaler(feature_range=(-1,1))
+        y_scaled = y_scaler.fit_transform(dataset[:,-1].reshape(-1, 1))
+
+        dataset = np.hstack((X_scaled, y_scaled))
+
+        X_train, y_train = [], []
+        for i in range(self.lookback_days, len(dataset) - self.lookahead_days + 1):
+                X_train.append(dataset[i - self.lookback_days:i, 0:dataset.shape[1] - 1])
+                y_train.append(dataset[i + self.lookahead_days - 1:i + self.lookahead_days, 5])
+
+        X_train, y_train = np.array(X_train), np.array(y_train)
+
+        return self.X_train, self.y_train, self.X_scaler, self.y_scaler
+
+
+    # ---- BUILD LSTM NEURAL NETWORK ----
+    def _build_default_model(self):
+        """
+        Creates training datasets based on the number of lookback days and prediction days.
+        
+                Parameters:
+                        data            (np.ndarray): training data
+                        lookback_days   (int): days considered for training
+                        n_epochs        (int): training epochs
+                        X_train         (np.ndarray): input values
+                        y_train         (np.ndarray): target values
+                Returns:
+                        model           (model obj): trained model
+                        history:        (history obj): model history/details
+        """
+        model = Sequential()
+        model.add(Conv1D(32, self.lookback_days-9, activation='relu', input_shape=(self.lookback_days, self.X_train.shape[2])))
+        model.add(MaxPooling1D(1,1))
+        model.add((LSTM(64, return_sequences=True)))
+        model.add((LSTM(32)))
+        model.add(Dense(16))
+        model.add(Dense(1))
+
+        model.compile(optimizer = Adam(learning_rate=0.01), loss='mean_squared_error')
+        
+        early_stop = EarlyStopping(monitor='val_loss', patience=20, verbose=0)
+
+        history = model.fit(self.X_train, self.y_train, epochs=self.epochs, callbacks=[early_stop],
+                                batch_size=64, validation_split=0.1, verbose=0)
+
+        return model, history
+
+
+    def _build_tuned_model(self, hp):
+        """
+
+        """
+        model = Sequential()
+        model.add(Conv1D(hp.Int(name='input_units_1', min_value=16, max_value=128, step=16), 
+                        hp.Int(name='input_units_2', min_value=16, max_value=128, step=16), activation='relu', input_shape=(self.lookback_days, self.X_train.shape[2])))
+        model.add(MaxPooling1D(1,1))
+        model.add((LSTM(hp.Int(name='lstm_1', min_value=16, max_value=128, step=16), return_sequences=True)))
+        model.add((LSTM(hp.Int(name='lstm_2', min_value=16, max_value=128, step=16))))
+        model.add(Dense(hp.Int(name='dense_1', min_value=16, max_value=128, step=16)))
+        model.add(Dense(1))
+        
+        model.compile(optimizer = Adam(learning_rate=hp.Float(name='learning_rate', min_value=0.001, max_value=0.1, step=0.001)), loss='mean_squared_error')
+        
+
+        return model
+
+    def _run_tuned_model(self):
+        """
+
+        """
+
+        early_stop = EarlyStopping(monitor='val_loss', patience=20, verbose=1)
+        
+        tuner = RandomSearch(
+                self._build_tuned_model,
+                objective='val_loss',
+                max_trials=3,
+                executions_per_trial=3,
+                )
+                
+        tuner.search(self.X_train, self.y_train, epochs=5, validation_split=0.1, callbacks=[early_stop])
+        best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+        model = tuner.hypermodel.build(best_hps)
+        history = model.fit(self.X_train, self.y_train, epochs=100, validation_split=0.1, callbacks=[early_stop])
+
+        return model, history
+
+    # ---- GENERATE PREDICTIONS ----
+    def _generate_predictions(self, model):
+        """
+        Creates training datasets based on the number of lookback days and prediction days.
+        
+                Parameters:
+                        data                       (np.ndarray): training data
+                        model                      (model obj): trained model
+                        scaler                     (scaler obj): scaler used to reverse transformed values
+                        dates                      (np.ndarray): input values
+                        n_future                   (int): number of future days predicted
+                        n_back                     (int): number of considered past days
+                        X_train                    (np.ndarray): input values
+                        y_train                    (np.ndarray): target values
+                Returns:
+                        mse                        (float): root mean squared error
+                        PREDICTIONS_FUTURE         (pd.DataFrame): future predictions
+                        PREDICTION_TRAIN           (pd.DataFrame): training predictions
+        """                    
+
+        all_predictions_train = model.predict(self.X_train)
+        all_y_train_pred_actual = self.y_scaler.inverse_transform(all_predictions_train)
+
+        predictions_train = model.predict(self.X_train[180:])
+        y_pred_train = self.y_scaler.inverse_transform(predictions_train)
+
+        predictions_future = model.predict(self.X_train[-45:])
+        y_pred_future = self.y_scaler.inverse_transform(predictions_future)
+
+        # Dates for future predictions
+        datelist_future = pd.date_range(list(self.dates)[-1], periods=self.lookahead_days, freq='1d').tolist()
+
+        # Final data frames for plotting
+        self.PREDICTIONS_FUTURE = pd.DataFrame(y_pred_future, columns=['close']).set_index(pd.Series(datelist_future))
+        self.PREDICTION_TRAIN = pd.DataFrame(y_pred_train, columns=['close']).set_index(pd.Series(self.dates[2 * self.lookback_days + self.lookahead_days - 1:]))
+        self.PREDICTION_TRAIN.index = pd.DatetimeIndex(PREDICTION_TRAIN.index)
+
+        # Calculate RMSE and R^2
+        rmse = math.sqrt(mean_squared_error(self.y_scaler.inverse_transform(self.y_train), all_y_train_pred_actual))
+        r2 = r2_score(self.y_scaler.inverse_transform(self.y_train), all_y_train_pred_actual)
+
+        return rmse, r2, self.PREDICTIONS_FUTURE, self.PREDICTION_TRAIN
+
+
+    def _pred_plot(self):
+        """
+        Creates training datasets based on the number of lookback days and prediction days.
+        
+                Parameters:
+                        df                       (pd.DataFrame): original dataframe
+                        dates                    (pd.Series): dates from dataframe
+                        start_date_plot          (string): date to start plot
+                        future_preds             (pd.DataFrame): future predictions
+                        train_preds              (pd.DataFrame): training predictions
+                Returns:
+                        st.pyplot(fig)
+        """
+
+        # Plot parameters
+        START_DATE_FOR_PLOTTING = '2019-02-15'
+
+        dates = list(self.dates)
+        self.df['Date'] = dates
+        self.df.set_index('Date', inplace=True)
+        self.df.index = pd.DatetimeIndex(self.df.index)
+
+        fig= plt.figure()
+        plt.plot(self.PREDICTIONS_FUTURE.index, self.PREDICTIONS_FUTURE['close'], color='r', label='Predicted Stock Price')
+        plt.plot(self.PREDICTION_TRAIN.loc[START_DATE_FOR_PLOTTING:].index, self.PREDICTION_TRAIN.loc[START_DATE_FOR_PLOTTING:]['close'], color='orange', label='Training predictions')
+        plt.plot(self.df.loc[START_DATE_FOR_PLOTTING:].index, self.df.loc[START_DATE_FOR_PLOTTING:]['close'], color='b', label='Actual Stock Price')
+        plt.axvline(x = min(self.PREDICTIONS_FUTURE.index), color='green', linewidth=2, linestyle='--')
+        plt.grid(which='major', color='#cccccc', alpha=0.5)
+
+        plt.legend(shadow=True)
+        plt.title('Predcitions and Actual Stock Prices', family='Arial', fontsize=18)
+        plt.xlabel('Timeline', family='Arial', fontsize=14)
+        plt.ylabel('Stock Price Value', family='Arial', fontsize=14)
+        plt.xticks(rotation=45, fontsize=8)
+        st.show(fig)
